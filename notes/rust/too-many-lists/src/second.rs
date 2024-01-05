a4b50a620edec33a2c2fd24c7cf8a88aa7a4f45b
Keisuke Shimokawa <shimoke4869@gmail.com>
2024-01-05 16:25:11 +0900

    [A Ok Stack] IntoIter の実装を追加

--- notes/rust/too-many-lists/src/second.rs
+++ notes/rust/too-many-lists/src/second.rs
@@ -32,14 +32,6 @@ impl<T> List<T> {
     }
 
     pub fn pop(&mut self) -> Option<T> {
-        // match self.head.take() {
-        //     None => None,
-        //     Some(node) => {
-        //         self.head = node.next;
-        //         Some(node.elem)
-        //     }
-        // }
-
         // Option<A> -> map(|A| B) -> Option<B>
         self.head.take().map(|node| {
             self.head = node.next;
@@ -62,6 +54,21 @@ impl<T> Drop for List<T> {
         while let Some(mut boxed_node) = cur_link {
             cur_link = boxed_node.next.take();
         }
+    }
+}
+
+pub struct IntoIter<T>(List<T>);
+
+impl<T> List<T> {
+    pub fn into_iter(self) -> IntoIter<T> {
+        IntoIter(self)
+    }
+}
+
+impl<T> Iterator for IntoIter<T> {
+    type Item = T;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.pop()
     }
 }
 
@@ -109,5 +116,19 @@ mod test {
 
         assert_eq!(list.peek(), Some(&42));
         assert_eq!(list.pop(), Some(42));
+    }
+
+    #[test]
+    fn into_iter() {
+        let mut list = List::new();
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        let mut iter = list.into_iter();
+        assert_eq!(iter.next(), Some(3));
+        assert_eq!(iter.next(), Some(2));
+        assert_eq!(iter.next(), Some(1));
+        assert_eq!(iter.next(), None);
     }
 }
