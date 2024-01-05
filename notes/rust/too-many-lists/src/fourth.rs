1b73ccea15551682e644c4dc4a67fbe974fcdd2c
Keisuke Shimokawa <shimoke4869@gmail.com>
2024-01-05 22:53:18 +0900

    [A Bad Safe Deque] 双方向リンクリストの実装を開始

---
+++ notes/rust/too-many-lists/src/fourth.rs
@@ -0,0 +1,14 @@
+use std::{cell::RefCell, rc::Rc};
+
+pub struct List<T> {
+    head: Link<T>,
+    tail: Link<T>,
+}
+
+type Link<T> = Option<Rc<RefCell<Node<T>>>>;
+
+struct Node<T> {
+    elem: T,
+    next: Link<T>,
+    prev: Link<T>,
+}
