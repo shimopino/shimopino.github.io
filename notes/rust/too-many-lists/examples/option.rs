af1b88296cba1a6e0a4d4e6733537ebbc39c0e49
Keisuke Shimokawa <shimoke4869@gmail.com>
2024-01-05 15:45:53 +0900

    [A Ok Stack] Option::map と Option::as_ref の組み合わせを追加

--- notes/rust/too-many-lists/examples/option.rs
+++ notes/rust/too-many-lists/examples/option.rs
@@ -1,8 +1,16 @@
 fn main() {
-    let some_string = Some(String::from("hello world"));
-    let some_string_len = some_string.map(|s| s.len());
+    let some_string1 = Some(String::from("hello world"));
+    let some_string_len1 = some_string1.as_ref().map(|s| s.len());
+
+    println!("original string: {:?}", some_string1);
+    println!("length string: {:?}", some_string_len1);
+
+    // --- --- ---
+
+    let some_string2 = Some(String::from("hello world"));
+    let some_string_len2 = some_string2.map(|s| s.len());
 
     // borrow of moved value: `some_string` value borrowed here after move
-    // println!("original string: {:?}", some_string);
-    println!("length string: {:?}", some_string_len);
+    // println!("original string: {:?}", some_string2);
+    println!("length string: {:?}", some_string_len2);
 }
