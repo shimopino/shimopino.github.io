10870718b2fe70a801a1ba48aa47fdc7430201dc
Keisuke Shimokawa <shimoke4869@gmail.com>
2024-01-06 14:43:00 +0900

    [A Bad Safe Deque] RefCell でのノードの先頭追加

--- notes/rust/too-many-lists/src/fourth.rs
+++ notes/rust/too-many-lists/src/fourth.rs
@@ -12,3 +12,43 @@ struct Node<T> {
     next: Link<T>,
     prev: Link<T>,
 }
+
+impl<T> Node<T> {
+    fn new(elem: T) -> Rc<RefCell<Self>> {
+        Rc::new(RefCell::new(Node {
+            elem,
+            next: None,
+            prev: None,
+        }))
+    }
+}
+
+impl<T> Default for List<T> {
+    fn default() -> Self {
+        List::new()
+    }
+}
+
+impl<T> List<T> {
+    pub fn new() -> Self {
+        List {
+            head: None,
+            tail: None,
+        }
+    }
+
+    pub fn push_front(&mut self, elem: T) {
+        let new_node = Node::new(elem);
+        match self.head.take() {
+            Some(old_node) => {
+                old_node.borrow_mut().prev = Some(Rc::clone(&new_node));
+                new_node.borrow_mut().next = Some(old_node);
+                self.head = Some(new_node);
+            }
+            None => {
+                self.tail = Some(Rc::clone(&new_node));
+                self.head = Some(new_node);
+            }
+        }
+    }
+}
