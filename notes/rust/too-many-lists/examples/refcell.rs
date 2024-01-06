ca4db8644c388cc0dc347d816d6e8e475ed2715c
Keisuke Shimokawa <shimoke4869@gmail.com>
2024-01-06 23:04:19 +0900

    [A Bad Safe Deque] Ref::map の例を追加

--- notes/rust/too-many-lists/examples/refcell.rs
+++ notes/rust/too-many-lists/examples/refcell.rs
@@ -1,7 +1,14 @@
-use std::{cell::RefCell, collections::HashMap, rc::Rc};
+use std::{
+    cell::{Ref, RefCell},
+    collections::HashMap,
+    rc::Rc,
+};
 
 fn main() {
     let shared_map = Rc::new(RefCell::new(HashMap::new()));
     shared_map.borrow_mut().insert("africa", 92388);
     assert_eq!(shared_map.borrow().get("africa"), Some(&92388));
+
+    let _ref_hash = shared_map.borrow();
+    let _ref_hash_map = Ref::map(_ref_hash, |hash| hash);
 }
